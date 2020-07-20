211e84a53c9cd69176adb747232c19c38e1521a2
Umut Zafer <umutzafer@Umuts-MacBook-Pro.local>
2020-07-21 00:48:10 +0300

    Signup eklendi

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import Login from './containers/Login';
 import NotFound from './containers/NotFound';
+import Signup from './containers/Signup';
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/login">
                 <Login />
+            </Route>
+            <Route exact path="/signup">
+                <Signup />
             </Route>
             <Route exact path="/">
                 <Home />
